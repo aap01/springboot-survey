96e2942a11ef57dd8f34fa6a477195353ad00223
aap <aap.pavel@gmail.com>
2020-10-01 12:29:27 +0600

    fixed auths

--- src/main/kotlin/com/aap/springboot_skeleton/adapter/SecurityConfigurerAdapter.kt
+++ src/main/kotlin/com/aap/springboot_skeleton/adapter/SecurityConfigurerAdapter.kt
@@ -5,21 +5,27 @@ import com.aap.springboot_skeleton.controller.PingController
 import com.aap.springboot_skeleton.service.MyUserDetailsService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.OncePerRequestFilter
 
 
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 class SecurityConfigurerAdapter : WebSecurityConfigurerAdapter() {
 
     @Autowired
@@ -32,15 +38,23 @@ class SecurityConfigurerAdapter : WebSecurityConfigurerAdapter() {
         auth?.userDetailsService(myUserDetailsService)
     }
 
+    @Throws(Exception::class)
+    override fun configure(web: WebSecurity) {
+        web.ignoring() //others if you need
+            .antMatchers(HttpMethod.OPTIONS, "/**")
+    }
+
+
     override fun configure(http: HttpSecurity?) {
-        http?.csrf()
-            ?.disable()
+        http?.cors()?.configurationSource(corsConfigurationSource())
+            ?.and()
+            ?.csrf()?.disable()
             ?.authorizeRequests()
-            ?.antMatchers(AuthController.LOGIN, AuthController.SIGN_UP, PingController.PING)?.permitAll()
+            ?.antMatchers(AuthController.LOGIN, AuthController.SIGN_UP, PingController.PING)
+            ?.permitAll()
             ?.anyRequest()?.authenticated()
             ?.and()
-            ?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        http?.addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter::class.java)
+            ?.addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter::class.java)
     }
 
 
@@ -52,6 +66,23 @@ class SecurityConfigurerAdapter : WebSecurityConfigurerAdapter() {
     @Bean
     fun passwordEncoder(): PasswordEncoder? {
         return BCryptPasswordEncoder()
+    }
+
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("*")
+        configuration.allowedMethods = listOf("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.allowCredentials = true
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.allowedHeaders = listOf("Authorization", "Cache-Control", "Content-Type")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 
 }
--- src/main/kotlin/com/aap/springboot_skeleton/controller/AuthController.kt
+++ src/main/kotlin/com/aap/springboot_skeleton/controller/AuthController.kt
@@ -11,12 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 @RestController
+@CrossOrigin
 class AuthController {
 
     companion object {
